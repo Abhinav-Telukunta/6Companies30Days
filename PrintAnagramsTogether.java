f7e4c9762672ef3cddf942fad1d24fc2810c8734
Telukunta Vijay Abhinav <62216101+Abhinav-Telukunta@users.noreply.github.com>
2022-01-06 10:50:27 +0530

    Jan-05

---
+++ PrintAnagramsTogether.java
@@ -0,0 +1,71 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main (String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	    int t=Integer.parseInt(br.readLine().trim());
+	    while(t > 0)
+	    {
+	        int n= Integer.parseInt(br.readLine().trim());
+	        String x = br.readLine().trim();
+	        String string_list[] = x.split(" ",n);
+	        
+	        Solution ob = new  Solution();
+	        
+	        List <List<String>> ans = ob.Anagrams(string_list);
+	        
+	        Collections.sort(ans, new Comparator<List<String>>(){
+            public int compare(List<String> l1, List<String> l2) {
+                    String s1 =  l1.get(0);
+                    String s2 = l2.get(0);
+                    
+                    return s1.compareTo(s2);
+                }
+            });
+	        
+	        for(int i=0;i<ans.size();i++)
+	        {
+	            for(int j=0;j<ans.get(i).size();j++)
+	            {
+	                System.out.print(ans.get(i).get(j) + " ");
+	            }
+	            System.out.println();
+	        }
+	       
+	       
+            t--;
+	    }
+	}
+    
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    public List<List<String>> Anagrams(String[] string_list) {
+        // Code here
+        Map<String,ArrayList<String>>map=new LinkedHashMap<>();
+        for(String str:string_list){
+            char[] charr=str.toCharArray();
+            Arrays.sort(charr);
+            String res=new String(charr);
+            map.computeIfAbsent(res,x->new ArrayList()).add(str);
+        }
+        List<List<String>>ans=new ArrayList<>();
+        for(Map.Entry<String,ArrayList<String>>entry:map.entrySet()){
+            List<String>value=entry.getValue();
+            ans.add(new ArrayList<>(value));
+        }
+        return ans;
+        
+    }
+}
