ca851c9efe60e4c0116b8aa7b890b382c47804fb
Telukunta Vijay Abhinav <62216101+Abhinav-Telukunta@users.noreply.github.com>
2022-01-16 14:35:37 +0530

    Jan-15

---
+++ MinimumStepsToDestination.java
@@ -0,0 +1,34 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            int D = Integer.parseInt(in.readLine());
+            
+            Solution ob = new Solution();
+            System.out.println(ob.minSteps(D));
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static int minSteps(int D){
+        // code here
+        int sum=0,step=0;
+        while(sum<D || (sum-D)%2!=0){
+            step++;
+            sum+=step;
+        }
+        return step;
+    }
+}
